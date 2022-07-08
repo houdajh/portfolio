7a3d70375e8c0c6b48f80a952bf4e5b49aae56a5
jouhar <jouharhouda2001@gmail.com>
2022-07-08 17:58:55 +0100

    myPortfolio

---
+++ backend_sanity/schemas/abouts.js
@@ -0,0 +1,38 @@
+export default{
+    name:'abouts',
+    title:'Abouts',
+    type: 'document',
+    fields:[
+        {
+            name:'title',
+            title:'Title',
+            type:'string'
+        },
+        {
+            name:'description',
+            title:'Description',
+            type:'string'
+        },
+        {
+            name:'birth',
+            title:'Date of birth',
+            type:'string'
+        },
+        {
+            name:'Address',
+            title:'Address',
+            type:'string'
+        },
+        {
+            name:'Email',
+            title:'Email',
+            type:'string'
+        },
+        {
+            name:'Phone',
+            title:'Phone',
+            type: 'string',
+        },
+        
+    ]
+}
---
+++ backend_sanity/schemas/works.js
@@ -0,0 +1,44 @@
+export default {
+    name: 'works',
+    title: 'Works',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+      {
+        name: 'codeLink',
+        title: 'Code Link',
+        type: 'string',
+      },
+      {
+        name: 'imgUrl',
+        title: 'ImageUrl',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+   
+      {
+        name: 'tags',
+        title: 'Tags',
+       type:'array',
+       of: [
+         {
+           name:'tag',
+           title:'Tag',
+           type:'string'
+         }
+       ]
+      },
+     
+    ],
+  };
